8df960a7e269b6bb0ccc0daf14a97ade08877bd0
JohannesBlankSFS <johannes.blank@sfs.de>
2025-12-16 14:20:45 +0100

    Unternehmensseite 99% Done!!!!

--- HonorarRechner.Wpf/HonorarRechner.Wpf/ViewModels/LeistungenViewModel.cs
+++ HonorarRechner.Wpf/HonorarRechner.Wpf/ViewModels/LeistungenViewModel.cs
@@ -11,30 +11,29 @@ namespace HonorarRechner.Wpf.ViewModels
     public class LeistungenViewModel : INotifyPropertyChanged
     {
         public event Action? ZurueckRequested;
-        public event Action? WeiterRequested;
+        // WeiterRequested entfernt/ignoriert, da es hier nicht weitergeht
         public event Action? NavigateToFibuRequested;
         public event Action? NavigateToJaRequested;
         public event Action? NavigateToLohnRequested;
 
         private readonly HonorarService _honorarService;
-        private readonly UnternehmensDaten _daten; // Datenfeld für direkten Zugriff
+        private readonly UnternehmensDaten _daten;
 
         public LeistungenViewModel()
         {
             _honorarService = new HonorarService();
             _daten = GlobalState.Instance.Daten;
 
-            // Checkbox-Status initial laden
             _fiBu = _daten.HatFiBu;
             _ja = _daten.HatJahresabschluss;
             _lohn = _daten.HatLohn;
             _selbstbucher = _daten.IstSelbstbucher;
 
-            // Commands
             OpenExcelCommand = new RelayCommand(_ => MessageBox.Show("Open Excel"));
             UpdateExcelCommand = new RelayCommand(_ => MessageBox.Show("Update Excel"));
             ZurueckCommand = new RelayCommand(_ => ZurueckRequested?.Invoke());
-            WeiterCommand = new RelayCommand(_ => WeiterRequested?.Invoke());
+
+            // WICHTIG: WeiterCommand existiert nicht mehr (bzw. ist null)
 
             NavigateToFibuCommand = new RelayCommand(_ => NavigateToFibuRequested?.Invoke());
             NavigateToJaCommand = new RelayCommand(_ => NavigateToJaRequested?.Invoke());
@@ -49,63 +48,20 @@ namespace HonorarRechner.Wpf.ViewModels
         public string JahresHonorarText => $"Jahres Honorar: {_jahresHonorar:C}";
         public string MonatsHonorarText => $"Monats Honorar: {(_jahresHonorar / 12m):C}";
 
-        // --- Checkbox Logic (Gegenseitiger Ausschluss) ---
-
+        // --- Checkboxen ---
         private bool _fiBu;
-        public bool FiBu
-        {
-            get => _fiBu;
-            set
-            {
-                if (Set(ref _fiBu, value))
-                {
-                    _daten.HatFiBu = value;
-
-                    // Wenn Fibu AN, muss Selbstbucher AUS
-                    if (value)
-                    {
-                        Selbstbucher = false;
-                    }
-                    Recalculate();
-                }
-            }
-        }
+        public bool FiBu { get => _fiBu; set { if (Set(ref _fiBu, value)) { _daten.HatFiBu = value; if (value) Selbstbucher = false; Recalculate(); } } }
 
         private bool _selbstbucher;
-        public bool Selbstbucher
-        {
-            get => _selbstbucher;
-            set
-            {
-                if (Set(ref _selbstbucher, value))
-                {
-                    _daten.IstSelbstbucher = value;
-
-                    // Wenn Selbstbucher AN, muss Fibu AUS
-                    if (value)
-                    {
-                        FiBu = false;
-                    }
-                    Recalculate();
-                }
-            }
-        }
+        public bool Selbstbucher { get => _selbstbucher; set { if (Set(ref _selbstbucher, value)) { _daten.IstSelbstbucher = value; if (value) FiBu = false; Recalculate(); } } }
 
         private bool _ja;
-        public bool JA
-        {
-            get => _ja;
-            set { if (Set(ref _ja, value)) { _daten.HatJahresabschluss = value; Recalculate(); } }
-        }
+        public bool JA { get => _ja; set { if (Set(ref _ja, value)) { _daten.HatJahresabschluss = value; Recalculate(); } } }
 
         private bool _lohn;
-        public bool Lohn
-        {
-            get => _lohn;
-            set { if (Set(ref _lohn, value)) { _daten.HatLohn = value; Recalculate(); } }
-        }
+        public bool Lohn { get => _lohn; set { if (Set(ref _lohn, value)) { _daten.HatLohn = value; Recalculate(); } } }
 
-        // --- Anzeige-Werte ---
+        // --- Anzeige Spalten ---
         public string FiBuMonatlichFormatted { get => _fiBuMonatlichFormatted; private set => Set(ref _fiBuMonatlichFormatted, value); }
         private string _fiBuMonatlichFormatted = "0,00 €";
         public string FiBuJaehrlichFormatted { get => _fiBuJaehrlichFormatted; private set => Set(ref _fiBuJaehrlichFormatted, value); }
@@ -121,33 +77,29 @@ namespace HonorarRechner.Wpf.ViewModels
         public string LohnJaehrlichFormatted { get => _lohnJaehrlichFormatted; private set => Set(ref _lohnJaehrlichFormatted, value); }
         private string _lohnJaehrlichFormatted = "0,00 €";
 
-        // Selbstbucher Spalte
         public string SelbstbucherMonatlichFormatted { get => _selbstbucherMonatlichFormatted; private set => Set(ref _selbstbucherMonatlichFormatted, value); }
         private string _selbstbucherMonatlichFormatted = "0,00 €";
         public string SelbstbucherJaehrlichFormatted { get => _selbstbucherJaehrlichFormatted; private set => Set(ref _selbstbucherJaehrlichFormatted, value); }
         private string _selbstbucherJaehrlichFormatted = "0,00 €";
 
-
         // --- Commands ---
         public ICommand OpenExcelCommand { get; }
         public ICommand UpdateExcelCommand { get; }
         public ICommand ZurueckCommand { get; }
-        public ICommand WeiterCommand { get; }
+
+        // HIER IST DER TRICK: WeiterCommand gibt NULL zurück -> Button wird unsichtbar
+        public ICommand? WeiterCommand => null;
+
         public ICommand NavigateToFibuCommand { get; }
         public ICommand NavigateToJaCommand { get; }
         public ICommand NavigateToLohnCommand { get; }
 
-
-        // --- Recalculate Logic ---
+        // --- Logic ---
         private void Recalculate()
         {
             var werte = GlobalState.Instance.Werte;
-
-            // 1. Footer Gesamt
             var gesamt = _honorarService.BerechneAlles();
             _jahresHonorar = gesamt.JahresHonorar;
-
-            // 2. Einzelwerte für Spalten
 
             // Fibu
             decimal fibuVal = _daten.HatFiBu ? _honorarService.BerechneFibu(_daten, werte) : 0;
@@ -169,18 +121,16 @@ namespace HonorarRechner.Wpf.ViewModels
             LohnMonatlichFormatted = (lohnVal / 12m).ToString("C");
             LohnJaehrlichFormatted = lohnVal.ToString("C");
 
-            // Selbstbucher (20% auf JA + Lohn, wenn aktiv)
+            // Selbstbucher
             decimal selbstbucherVal = 0;
             if (_daten.IstSelbstbucher)
             {
-                // Hier auch für die Vorschau die gleiche Logik: (JA + Lohn) * 20%
                 decimal basis = jaVal + lohnVal;
                 selbstbucherVal = basis * 0.20m;
             }
             SelbstbucherMonatlichFormatted = (selbstbucherVal / 12m).ToString("C");
             SelbstbucherJaehrlichFormatted = selbstbucherVal.ToString("C");
 
-            // UI Refresh
             OnPropertyChanged(nameof(JahresHonorarText));
             OnPropertyChanged(nameof(MonatsHonorarText));
         }
